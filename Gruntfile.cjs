62ac467e6394f5e12f4ddb85e1616d962f55e336
CShah18 <chirageng.webforest@gmail.com>
2025-02-27 14:34:47 +0530

    first commit

---
+++ Gruntfile.cjs
@@ -0,0 +1,38 @@
+const grunt = require('grunt');
+
+grunt.initConfig({
+  bump: {
+    options: {
+      files: ['package.json'],
+      updateConfigs: [],
+      commit: true,
+      commitMessage: 'Release v%VERSION%',
+      commitFiles: ['package.json'],
+      createTag: true,
+      tagName: 'v%VERSION%',
+      tagMessage: 'Version %VERSION%',
+      push: true,
+      // pushTo: 'upstream',
+      pushTo: 'origin',
+      gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d',
+      globalReplace: false,
+      prereleaseName: false,
+      metadata: '',
+      regExp: false
+    }
+  },
+  pkg: grunt.file.readJSON('package.json'),
+  uglify: {
+    options: {
+      banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+    },
+    build: {
+      src: 'src/<%= pkg.name %>.js',
+      dest: 'build/<%= pkg.name %>.min.js'
+    }
+  }
+});
+
+grunt.loadNpmTasks('grunt-bump');
+grunt.loadNpmTasks('grunt-contrib-uglify');
+grunt.registerTask('default', ['uglify']);
